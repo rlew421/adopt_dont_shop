9ec7e60a0eaff55649580b557f3b58d308daf0ed
Rachel Lew <rlew07@gmail.com>
2019-11-20 10:10:47 -0700

    Migrate description and adoptable? columns into pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191120024424) do
+ActiveRecord::Schema.define(version: 20191120164447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20191120024424) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
+    t.string "description"
+    t.boolean "adoptable?", default: true
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 

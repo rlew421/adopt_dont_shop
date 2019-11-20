2272c9269d8a3b73c221138b719ea8e1294a3852
Rachel Lew <rlew07@gmail.com>
2019-11-20 11:26:33 -0700

    Add tests to validate presence of description and inclusion of adoptable?

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -7,6 +7,8 @@ describe Pet, type: :model do
     it {should validate_presence_of(:approximate_age)}
     it {should validate_presence_of(:sex)}
     it {should validate_presence_of(:current_shelter)}
+    it {should validate_presence_of(:description)}
+    it {should validate_inclusion_of(:adoptable?).in_array([true,false])}
   end
 
   describe "relationships" do

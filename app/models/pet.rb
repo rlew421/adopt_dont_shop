df7e0fc5a66380719f616052625f520cf08c1da2
Rachel Lew <rlew07@gmail.com>
2019-11-20 11:28:13 -0700

    Add validations for description and adoptable? status

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,10 @@
 class Pet < ApplicationRecord
-  validates_presence_of :image, :name, :approximate_age, :sex, :current_shelter
+  validates_presence_of  :image,
+                         :name,
+                         :approximate_age,
+                         :sex,
+                         :current_shelter,
+                         :description
+  validates_inclusion_of :adoptable?, :in => [true, false]
   belongs_to :shelter
 end

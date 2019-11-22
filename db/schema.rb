78229b6c81e858c19493020073af8e3b71c7bef8
Rachel Lew <rlew07@gmail.com>
2019-11-22 12:39:52 -0700

    Migrate migration that removes current_shelter column from pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191120164447) do
+ActiveRecord::Schema.define(version: 20191122192824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20191120164447) do
     t.string "name"
     t.integer "approximate_age"
     t.string "sex"
-    t.string "current_shelter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"

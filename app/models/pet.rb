5ee94d20082a37a666326967736a9f45559f6963
Rachel Lew <rlew07@gmail.com>
2019-11-22 12:38:42 -0700

    Remove current_shelter validation from Pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,7 +3,6 @@ class Pet < ApplicationRecord
                          :name,
                          :approximate_age,
                          :sex,
-                         :current_shelter,
                          :description
   validates_inclusion_of :adoptable?, :in => [true, false]
   belongs_to :shelter
